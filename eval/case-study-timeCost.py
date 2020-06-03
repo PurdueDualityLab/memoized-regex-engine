c049569d257b799f855461c693d6cfac1d4ddcb5
Jamie Davis <davisjam@vt.edu>
2020-06-03 17:55:03 -0400

    update case study code -- one plot per regex

--- eval/case-study-timeCost.py
+++ eval/case-study-timeCost.py
@@ -32,14 +32,115 @@ shellDeps = [ libMemo.ProtoRegexEngine.CLI ]
 SAVE_TMP_FILES = True
 
 # Other globals
-MAX_MATCH_MS = 1*1000 # TODO
-NTRIALS = 5 # Take the median of this many trials
+MAX_MATCH_MS = 5*1000
+NTRIALS = 3 # Take the median of this many trials
 
-def loadRegexFile(regexFile):
-  """Return a list of (nick, libMemo.SimpleRegex)"""
-  regexes = []
-  libLF.log('Loading regexes from {}'.format(regexFile))
-  with open(regexFile, 'r') as inStream:
+class CaseStudy:
+  expPumps = list(range(1,35,1))
+  polyPumps = [1,2,3,5,8] + list(range(25,1000,25))
+  linPumps = [1,2,3,5,8] + list(range(100,1000,100))
+
+  def __init__(self, line):
+    obj = json.loads(line)
+    self.nick = obj['nick']
+    self.title = obj['title']
+    self.unmemoBehav = obj['unmemoBehav']
+    self.memoBehav = obj['memoBehav']
+    self.regex = libMemo.SimpleRegex()
+    self.regex.initFromNDJSON(line)
+  
+  def _collectData(self, pumps, doMemo):
+    matchTimes = []
+
+    if doMemo:
+      ss = libMemo.ProtoRegexEngine.SELECTION_SCHEME.SS_Full
+    else:
+      ss = libMemo.ProtoRegexEngine.SELECTION_SCHEME.SS_None
+    es = libMemo.ProtoRegexEngine.ENCODING_SCHEME.ES_None
+
+    matchTimeMS = 0
+    for pump in pumps:
+      if matchTimeMS > MAX_MATCH_MS:
+        break
+
+      times = []
+      # Query
+      libLF.log("nick {} nPumps {}".format(self.nick, pump))
+      queryFile = libMemo.ProtoRegexEngine.buildQueryFile(self.regex.pattern, self.regex.evilInputs[0].build(pump))
+      for i in range(NTRIALS):
+        em = libMemo.ProtoRegexEngine.query(ss, es, queryFile)
+        matchTimeMS = max(int(em.si_simTimeUS/1000), 10)
+        times.append(matchTimeMS)
+      med = statistics.median(times)
+      os.unlink(queryFile)
+
+      # Record
+      matchTimes.append(med)
+      libLF.log("nPumps {} matchTimeMS {}".format(pump, matchTimes[-1]))
+    return matchTimes
+
+  def run(self):
+    """Plot the "growth curve" for this case study, with and without memoization
+
+    Returns: name of file
+    """
+    # Track these for the second pass
+
+    nonMemoMatchTimes = []
+  
+    if self.unmemoBehav == "exp":
+      unmemoPumps = CaseStudy.expPumps
+    elif self.unmemoBehav == "poly":
+      unmemoPumps = CaseStudy.polyPumps
+    elif self.unmemoBehav == "lin":
+      unmemoPumps = CaseStudy.linPumps
+
+    if self.memoBehav == "exp":
+      memoPumps = CaseStudy.expPumps
+    elif self.memoBehav == "poly":
+      memoPumps = CaseStudy.polyPumps
+    elif self.memoBehav == "lin":
+      memoPumps = CaseStudy.linPumps
+
+    unmemoMatchTimes = self._collectData(unmemoPumps, False)
+    memoMatchTimes = self._collectData(memoPumps, True)
+
+    ret = []
+    for np, mt in zip(unmemoPumps, unmemoMatchTimes):
+      ret.append({"Memoized": False, "nPumps": np, "matchTimeS": mt/1000})
+    for np, mt in zip(memoPumps, memoMatchTimes):
+      ret.append({"Memoized": True, "nPumps": np, "matchTimeS": mt/1000})
+    
+    #### DataFrame
+    df = pd.DataFrame(data=ret)
+    print(df.groupby("Memoized").describe())
+
+    #### Emit results
+    caseStudy_fname = os.path.join('figs', 'caseStudy-{}.pdf'.format(self.nick))
+    plt.figure()
+    ax = sns.lineplot(x="nPumps", y="matchTimeS", hue="Memoized", hue_order=[False, True], marker='o', data=df)
+    ax.set_ylim(bottom=0, top=MAX_MATCH_MS/1000)
+    if self.memoBehav == "lin":
+      ax.set_xlim(left=0, right=250)
+    elif self.memoBehav == "poly":
+      pass
+
+    plt.ylabel('Match time (s)', fontsize=22)
+    plt.yticks(fontsize=20)
+    plt.xlabel('Number of pumps (string length)', fontsize=22)
+    plt.xticks(fontsize=20)
+    plt.title(self.title, fontsize=24)
+    ax.legend(loc="lower right", fontsize=20)
+    print("Saving to {}".format(caseStudy_fname))
+    plt.savefig(fname=caseStudy_fname, bbox_inches='tight')
+
+    return caseStudy_fname
+
+def loadCaseStudies(caseStudyFile):
+  """Return CaseStudy[]"""
+  caseStudies = []
+  libLF.log('Loading caseStudies from {}'.format(caseStudyFile))
+  with open(caseStudyFile, 'r') as inStream:
     for line in inStream:
       line = line.strip()
       if len(line) == 0:
@@ -47,136 +148,47 @@ def loadRegexFile(regexFile):
       
       try:
         # Build a Regex
-        nick = json.loads(line)['nick']
-        if nick == 'Baseline':
-          continue
-        regex = libMemo.SimpleRegex()
-        regex.initFromNDJSON(line)
-        regexes.append((nick, regex))
+        caseStudies.append(CaseStudy(line))
       except KeyboardInterrupt:
         raise
       except BaseException as err:
         libLF.log('Exception parsing line:\n  {}\n  {}'.format(line, err))
         traceback.print_exc()
 
-    libLF.log('Loaded {} regexes from {}'.format(len(regexes), regexFile))
-    return regexes
+    libLF.log('Loaded {} caseStudies from {}'.format(len(caseStudies), caseStudyFile))
+    return caseStudies
 
 ################
 
-def evalRegex(nick, regex):
-  """Build the "growth curve" for this regex, with and without memoization
-
-  Returns: data[] dicts with keys: {nPumps, nonMemoMatchTimeMS, memoMatchTimeMS}, sorted by increasing nPumps
-  """
-  # Track these for the second pass
-  nPumps = []
-  nonMemoMatchTimes = []
-
-  pumpsToTry = list(range(1, 10, 1)) + list(range(10, 30, 2)) + list(range(50, 100, 10)) + list(range(100, 200, 25)) + list(range(200, 1000, 100)) + list(range(1000, 10000, 1000))
-
-  matchTimeMS = 0
-  pumpIx = 0
-  while matchTimeMS < MAX_MATCH_MS and pumpIx < len(pumpsToTry):
-    times = []
-    # Query
-    libLF.log("nick {} nPumps {} prev matchTimeMS {}".format(nick, pumpsToTry[pumpIx], matchTimeMS))
-    queryFile = libMemo.ProtoRegexEngine.buildQueryFile(regex.pattern, regex.evilInputs[0].build(pumpsToTry[pumpIx]))
-    for i in range(NTRIALS):
-      em = libMemo.ProtoRegexEngine.query(libMemo.ProtoRegexEngine.SELECTION_SCHEME.SS_None, libMemo.ProtoRegexEngine.ENCODING_SCHEME.ES_None, queryFile)
-      matchTimeMS = max(int(em.si_simTimeUS/1000), 10)
-      times.append(matchTimeMS)
-    matchTimeMS = statistics.median(times)
-    os.unlink(queryFile)
-
-    # Record
-    nonMemoMatchTimes.append(matchTimeMS)
-    nPumps.append(pumpsToTry[pumpIx])
-    libLF.log("nPumps {} matchTimeMS {}".format(nPumps[-1], nonMemoMatchTimes[-1]))
-
-    # Grow
-    pumpIx += 1
-  
-  # Now try with memoization
-  libLF.log("Now trying with memoization")
-  memoMatchTimes = []
-  for currPump in nPumps:
-    # Query
-    queryFile = libMemo.ProtoRegexEngine.buildQueryFile(regex.pattern, regex.evilInputs[0].build(currPump))
-
-    times = []
-    for i in range(NTRIALS):
-      em = libMemo.ProtoRegexEngine.query(libMemo.ProtoRegexEngine.SELECTION_SCHEME.SS_InDeg, libMemo.ProtoRegexEngine.ENCODING_SCHEME.ES_None, queryFile)
-      matchTimeMS = max(int(em.si_simTimeUS/1000), 10)
-      times.append(matchTimeMS)
-    matchTimeMS = statistics.median(times)
-    os.unlink(queryFile)
-
-    # Record
-    memoMatchTimes.append(matchTimeMS)
-
-  return [
-    {"nPumps": np, "nonMemoMatchTimeMS": nmmt, "memoMatchTimeMS": mmt}
-    for np, nmmt, mmt in zip(nPumps, nonMemoMatchTimes, memoMatchTimes)
-  ]
-
-def main(regexFile):
-  libLF.log('regexFile {}' \
-    .format(regexFile))
+def main(caseStudyFile):
+  libLF.log('caseStudyFile {}' \
+    .format(caseStudyFile))
 
   #### Check dependencies
   libLF.checkShellDependencies(shellDeps)
 
   #### Load data
-  regexes = loadRegexFile(regexFile)
-  print([nick for nick, reg in regexes])
+  caseStudies = loadCaseStudies(caseStudyFile)
 
-  #### Collect data
-  nick2dat = {}
-  for nick, regex in regexes:
-    nick2dat[nick] = evalRegex(nick, regex)
-  
-  ### Munge into DF
-  maxPumpsForNonMemoMatchTime = 0
-  records = []
-  for nick, dat in nick2dat.items():
-    for rec in dat:
-      records += [
-        {"Regex": nick, "Memoized": False, "nPumps": rec["nPumps"], "matchTimeMS": rec["nonMemoMatchTimeMS"] },
-        {"Regex": nick, "Memoized": True, "nPumps": rec["nPumps"], "matchTimeMS": rec["memoMatchTimeMS"] },
-      ]
-      if nick != "Baseline":
-        maxPumpsForNonMemoMatchTime = max(rec["nPumps"], maxPumpsForNonMemoMatchTime)
-      print(records[-2:])
-  df = pd.DataFrame(columns=['Regex', 'Memoized', 'nPumps', 'matchTimeMS'], data=records)
-  print(df.describe())
+  #### Run
+  studiesToRun = ["REWBR-2", "Microsoft", "Cloudflare"]
+  nick2fname = {}
+  for caseStudy in caseStudies:
+    if caseStudy.nick in studiesToRun:
+      nick2fname[caseStudy.nick] = caseStudy.run()
+  for nick, fname in nick2fname.items():
+    libLF.log("Case study {} -- See {}".format(nick, fname))
 
-  #### Emit results
-  caseStudy_fname = os.path.join('figs', 'caseStudy.pdf')
-  plt.figure(1)
-  ax = sns.lineplot(x='nPumps', y='matchTimeMS',
-        hue='Regex',
-        style='Memoized',
-        marker='o',
-        data=df)
-  ax.set_ylim(bottom=0, top=MAX_MATCH_MS)
-  ax.set_xlim(left=0, right=maxPumpsForNonMemoMatchTime)
-
-  ax.set_ylabel('Match time (ms)')
-  ax.set_xlabel('Number of pumps')
-  plt.title('Case studies with and without memoization')
-  print("Saving to {}".format(caseStudy_fname))
-  plt.savefig(fname=caseStudy_fname, bbox_inches='tight')
 
 #####################################################
 
 # Parse args
 parser = argparse.ArgumentParser(description='Measure the dynamic costs of memoization -- the space and time costs of memoizing this set of regexes, as determined using the prototype engine.')
-parser.add_argument('--regex-file', type=str, help='In: NDJSON file of objects containing libMemo.SimpleRegex objects (at least the key "pattern", and "evilInput" if you want an SL-specific analysis)', required=True,
-  dest='regexFile')
+parser.add_argument('--caseStudy-file', type=str, help='In: NDJSON file of objects containing CaseStudy objects (keys: nick, title, unmemoBehav, memoBehav, pattern, evilInputs)', required=True,
+  dest='caseStudyFile')
 
 # Parse args
 args = parser.parse_args()
 
 # Here we go!
-main(args.regexFile)
+main(args.caseStudyFile)
